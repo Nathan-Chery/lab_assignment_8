70d9ab4e2e8d82b5efe09498f7ba476c8e29f7e8
Nathan Chery <nathanchery181@gmail.com>
2023-04-05 14:07:03 -0400

    Successful

--- sort.c
+++ sort.c
@@ -1,20 +1,135 @@
 #include <time.h>
 #include <stdlib.h>
 #include <stdio.h>
+#include <string.h>
 
 int extraMemoryAllocated;
 
-// implements heap sort
-// extraMemoryAllocated counts bytes of memory allocated
-void heapSort(int arr[], int n)
+void heapify(int arr[], int n, int i) 
 {
+    int largest = i;  // Initialize largest as root
+    int l = 2*i + 1;  // left = 2*i + 1
+    int r = 2*i + 2;  // right = 2*i + 2
+ 
+    // If left child is larger than root
+    if (l < n && arr[l] > arr[largest])
+        largest = l;
+ 
+    // If right child is larger than largest so far
+    if (r < n && arr[r] > arr[largest])
+        largest = r;
+ 
+    // If largest is not root
+    if (largest != i) {
+        // Swap arr[i] and arr[largest] using a temporary variable
+        int temp = arr[i];
+        arr[i] = arr[largest];
+        arr[largest] = temp;
+ 
+        // Recursively heapify the affected sub-tree
+        heapify(arr, n, largest);
+    }
 }
 
+ // implements heap sort
+// extraMemoryAllocated counts bytes of memory allocated
+void heapSort(int arr[], int n) 
+{
+    // Build heap (rearrange array)
+    for (int i = n / 2 - 1; i >= 0; i--)
+        heapify(arr, n, i);
+ 
+    // One by one extract an element from heap
+    for (int i = n-1; i >= 0; i--) {
+        // Move current root to end
+        int temp = arr[0];
+        arr[0] = arr[i];
+        arr[i] = temp;
+ 
+        // call max heapify on the reduced heap
+        heapify(arr, i, 0);
+    }
+}
+
+void merge(int arr[], int l, int m, int r)
+{
+	int i, j, k;
+	int n1 = m - l + 1;
+	int n2 = r - m;
+
+	/* create temp arrays */
+	int *L = (int*) malloc(n1*sizeof(int));
+	int *R = (int*) malloc(n2*sizeof(int));
+
+	extraMemoryAllocated += n1 * sizeof(int) + n2 * sizeof(int);
+
+	/* Copy data to temp arrays L[] and R[] */
+	for (i = 0; i < n1; i++)
+	{
+		L[i] = arr[l + i];
+	}
+	
+
+	for (j = 0; j < n2; j++)
+	{
+		R[j] = arr[m + 1+ j];
+	}
+	
+	/* Merge the temp arrays back into arr[l..r]*/
+	i = 0; // Initial index of first subarray
+	j = 0; // Initial index of second subarray
+	k = l; // Initial index of merged subarray
+
+	while (i < n1 && j < n2)
+	{
+		if (L[i] <= R[j])
+		{
+		arr[k] = L[i];
+		i++;
+		}
+		else
+		{
+		arr[k] = R[j];
+		j++;
+		}
+	k++;
+	}
+
+/* Copy the remaining elements of L[], if there
+are any */
+	while (i < n1)
+	{
+		arr[k] = L[i];
+		i++;
+		k++;
+	}
+/* Copy the remaining elements of R[], if there
+are any */
+	while (j < n2)
+	{
+		arr[k] = R[j];
+		j++;
+		k++;
+	}
+	free(L);
+	free(R);
+}
 
 // implement merge sort
 // extraMemoryAllocated counts bytes of extra memory allocated
-void mergeSort(int pData[], int l, int r)
+void mergeSort(int arr[], int l, int r)
 {
+	if (l < r)
+	{
+		// get the mid point
+		int m = (l+r)/2;
+		// Sort first and second halves
+		mergeSort(arr, l, m);
+		mergeSort(arr, m+1, r);
+		
+		//merges the sub arrays together again
+		merge(arr, l, m, r);
+	}
 }
 
 // parses input file to an integer array
@@ -55,6 +170,12 @@ void printArray(int pData[], int dataSz)
 	printf("\tData:\n\t");
 	for (i=0;i<100;++i)
 	{
+		if (i>= dataSz)
+		{
+			printf("\n\n");
+			return;
+		}
+			
 		printf("%d ",pData[i]);
 	}
 	printf("\n\t");
